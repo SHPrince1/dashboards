d53fa9335eb77f1a0c45eda8ed9db5ca1225fca9
Sylvanus Prince <princehsylvanus@gmail.com>
2022-10-21 12:07:39 +0100

    new

--- src/components/sidebaritem.js
+++ src/components/sidebaritem.js
@@ -1,17 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import SbItem from "../styles/sidebaritem.module.css";
 import * as Icon from "react-bootstrap-icons";
 import { IoIosArrowDropdownCircle } from "react-icons/io";
 import { AiFillSetting } from "react-icons/ai";
 
 const Sidebaritem = () => {
+  const [open, Setopen] = useState(false);
+
+  function toggleOpen() {
+    Setopen(!open);
+  }
   return (
-    <div className={SbItem.sidebarItems}>
-      <div className={SbItem.sidebarC}>
-        <AiFillSetting size={30} />
-        <p>General</p>
-        <IoIosArrowDropdownCircle size={30}/>
+    // <div className={open ? "SbItem.sidebarItems open" : "SbItem.sidebarItems"}>
+    //   <div className={SbItem.sidebarTitle}>
+    //     <span >
+    //       <IoIosArrowDropdownCircle className={SbItem.toggleBtn} />
+    //       General
+    //     </span>
+
+    //     <span>
+    //       <AiFillSetting />
+    //     </span>
+    //   </div>
+    //  <div className={SbItem.sidebarContent}>
+    //   hello
+
+    //  </div>
+
+    // </div>
+
+    <div className={SbItem.parentBox}>
+      <div className={SbItem.genBox}>
+        <div className={SbItem.genTitle} onClick={toggleOpen}>
+          <AiFillSetting />
+          GENERAL
+          <IoIosArrowDropdownCircle />
+        </div>
+        <div className= {open ?  SbItem.contents :  SbItem.hide}>
+          General CONTENTS
+          General CONTENTS
+          General CONTENTS
+        
+        </div>
       </div>
+
+      <div className={SbItem.genBox}>
+        <div className={SbItem.genTitle} >
+          <AiFillSetting />
+          ACCOUNT
+          <IoIosArrowDropdownCircle />
+        </div>
+        <div className= {SbItem.contents }>
+          General CONTENTS
+          General CONTENTS
+          General CONTENTS
+        
+        </div>
+      </div>
+      
     </div>
   );
 };
